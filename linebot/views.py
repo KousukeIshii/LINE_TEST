c27d4eac8870e033de4124e09fc5e6f61af2ae7e
石井康介 <faitayahhuu@yahoo.co.jp>
2018-06-02 15:23:22 +0900

    fix indent views.py

--- linebot/views.py
+++ linebot/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-	REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
-	ACCESS_TOKEN = "nEv5t8dy06W+UT3NVcK7OPiEgDbXCTwswcBn0GgkflmT6SMZ1fZWYM3mp8tM3a5TIcZGWh3iA1U1N1iQ4kHoTByBD156IetsDIGqGcgOaEN0WL3lO3faf2ShLBBZpvvAbtE7E5EFbsc6wrXFtbR3mgdB04t89/1O/w1cDnyilFU="
-	HEADER = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + ACCESS_TOKEN
-	}
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+ACCESS_TOKEN = "nEv5t8dy06W+UT3NVcK7OPiEgDbXCTwswcBn0GgkflmT6SMZ1fZWYM3mp8tM3a5TIcZGWh3iA1U1N1iQ4kHoTByBD156IetsDIGqGcgOaEN0WL3lO3faf2ShLBBZpvvAbtE7E5EFbsc6wrXFtbR3mgdB04t89/1O/w1cDnyilFU="
+HEADER = {
+"Content-Type": "application/json",
+"Authorization": "Bearer " + ACCESS_TOKEN
+}
 	
 # Create your views here.
 def index(request):
