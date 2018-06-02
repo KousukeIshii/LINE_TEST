8e56a72a451cf5200b3422ee2363290936d3f61c
石井康介 <faitayahhuu@yahoo.co.jp>
2018-06-02 15:28:33 +0900

    fix indent views.py

--- linebot/views.py
+++ linebot/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN = "nEv5t8dy06W+UT3NVcK7OPiEgDbXCTwswcBn0GgkflmT6SMZ1fZWYM3mp8tM3a5TIcZGWh3iA1U1N1iQ4kHoTByBD156IetsDIGqGcgOaEN0WL3lO3faf2ShLBBZpvvAbtE7E5EFbsc6wrXFtbR3mgdB04t89/1O/w1cDnyilFU="
 HEADER = {
-"Content-Type": "application/json",
-"Authorization": "Bearer " + ACCESS_TOKEN
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + ACCESS_TOKEN
 }
 	
 # Create your views here.
@@ -29,5 +29,3 @@ def callback(request):
             	]
             }
             requests.post(REPLY_ENDPOINT, headers=HEADER, data=json.dumps(reply))
-            
-
