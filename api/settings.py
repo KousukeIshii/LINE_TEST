388e1497c1f44472ab43f92b1296a7d8a32c5a42
石井康介 <faitayahhuu@yahoo.co.jp>
2018-06-02 14:58:47 +0900

    edit setting for heroku

--- api/settings.py
+++ api/settings.py
@@ -119,3 +119,14 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mighty-ocean-34181.herokuapp.com']
